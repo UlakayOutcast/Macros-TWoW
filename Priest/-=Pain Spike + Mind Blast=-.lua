a818a7b6b97c0cb7425566964efcebabf11aaa2a
Ulukay <95168076+UlakayOutcast@users.noreply.github.com>
2025-06-27 21:06:19 +0700

    Add files via upload

---
+++ Priest/-=Pain Spike + Mind Blast=-.lua
@@ -0,0 +1,50 @@
+-=Pain Spike + Mind Blast=-
+/script local SE,Rank,spellName,spellRank=0;if nil then CastSpellByName("Mind Blast");end;local function ActSpell(spell,rank,sw) local ix=192;while(ix>0) do if ix==0 then return nil;else spellName,spellRank=GetSpellName(ix,"spell");if spellName==spell and (spellRank==rank or rank==nil) then if sw==true then return ix;end;if sw==0 then return GetSpellCooldown(ix,"spell");end;if sw==nil then if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,'spell');return ix;end;end;end;ix=ix-1;end;end;end;local function GotBuff(name,target) local tex,cnt,ix;for ix = 1,32 do tex,cnt = UnitBuff("player",ix);if not tex then return end;if strfind(tex,name) then return cnt end;end;end;function castSmite()local PM=UnitMana("player");if not UnitCanAttack("player","target") or UnitHealth("target")==0 then TargetNearestEnemy();end;if UnitCanAttack("player","target") and UnitHealth("target")>0 then local TH=UnitHealth("target");if PM>=265 and TH<=378 and ActSpell("Pain Spike","Rank 4",true) then SpellStopCasting(); if ActSpell("Pain Spike","Rank 4") then return;end;end;if PM>=185 and TH<=240 and ActSpell("Pain Spike","Rank 3",true) then SpellStopCasting(); if ActSpell("Pain Spike","Rank 3") then return;end;end;if PM>=140 and TH<=172 and ActSpell("Pain Spike","Rank 2",true) then SpellStopCasting(); if ActSpell("Pain Spike","Rank 2") then return;end;end;if PM>=80 and TH<=85 and ActSpell("Pain Spike","Rank 1",true) then SpellStopCasting(); if ActSpell("Pain Spike","Rank 1") then return;end;end;if PM>=350 and ActSpell("Mind Blast","Rank 9") then return;end;if PM>=310 and ActSpell("Mind Blast","Rank 8") then return;end;if PM>=265 and ActSpell("Mind Blast","Rank 7") then return;end;if PM>=225 and ActSpell("Mind Blast","Rank 6") then return;end;if PM>=185 and ActSpell("Mind Blast","Rank 5") then return;end;if PM>=150 and ActSpell("Mind Blast","Rank 4") then return;end;if PM>=110 and ActSpell("Mind Blast","Rank 3") then return;end;if PM>=80 and ActSpell("Mind Blast","Rank 2") then return;end;if PM>=50 and ActSpell("Mind Blast","Rank 1") then return;end;end;end;castSmite();
+
+/script local SE,Rank,spellName,spellRank=0;if nil then CastSpellByName("Mind Blast");end;
+local function ActSpell(spell,rank,sw) 
+	local ix=192;
+	while(ix>0) do 
+		if ix==0 then return nil;
+		else 
+			spellName,spellRank=GetSpellName(ix,"spell");
+			if spellName==spell and (spellRank==rank or rank==nil) then 
+				if sw==true then return ix;end;
+				if sw==0 then return GetSpellCooldown(ix,"spell");end;
+				if sw==nil then 
+					if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,'spell');return ix;end;
+				end;
+			end;
+			ix=ix-1;
+		end;
+	end;
+end;
+local function GotBuff(name,target) 
+	local tex,cnt,ix;
+	for ix = 1,32 do 
+		tex,cnt = UnitBuff("player",ix);
+		if not tex then return end;
+		if strfind(tex,name) then return cnt end;
+	end;
+end;
+function castSmite()
+	local PM=UnitMana("player");
+	if not UnitCanAttack("player","target") or UnitHealth("target")==0 then TargetNearestEnemy();end;
+	if UnitCanAttack("player","target") and UnitHealth("target")>0 then 
+		local TH=UnitHealth("target");
+		if PM>=265 and TH<=378 and ActSpell("Pain Spike","Rank 4",true) then SpellStopCasting(); if ActSpell("Pain Spike","Rank 4") then return;end;end;
+		if PM>=185 and TH<=240 and ActSpell("Pain Spike","Rank 3",true) then SpellStopCasting(); if ActSpell("Pain Spike","Rank 3") then return;end;end;
+		if PM>=140 and TH<=172 and ActSpell("Pain Spike","Rank 2",true) then SpellStopCasting(); if ActSpell("Pain Spike","Rank 2") then return;end;end;
+		if PM>=80 and TH<=85 and ActSpell("Pain Spike","Rank 1",true) then SpellStopCasting(); if ActSpell("Pain Spike","Rank 1") then return;end;end;
+		if PM>=350 and ActSpell("Mind Blast","Rank 9") then return;end;
+		if PM>=310 and ActSpell("Mind Blast","Rank 8") then return;end;
+		if PM>=265 and ActSpell("Mind Blast","Rank 7") then return;end;
+		if PM>=225 and ActSpell("Mind Blast","Rank 6") then return;end;
+		if PM>=185 and ActSpell("Mind Blast","Rank 5") then return;end;
+		if PM>=150 and ActSpell("Mind Blast","Rank 4") then return;end;
+		if PM>=110 and ActSpell("Mind Blast","Rank 3") then return;end;
+		if PM>=80 and ActSpell("Mind Blast","Rank 2") then return;end;
+		if PM>=50 and ActSpell("Mind Blast","Rank 1") then return;end;
+	end;
+end;
+castSmite();
--- Priest/-=Smite+Sun's Embrace=-.lua
+++ Priest/-=Smite+Sun's Embrace=-.lua
@@ -1,110 +1,98 @@
--=Smite+Sun's Embrace=-
-/script local ix,SE,Rank,spellName,spellRank;local function ActSpell(spell,rank,sw) ix=192;while(ix>0) do if ix==0 then return nil;else spellName,spellRank=GetSpellName(ix,"spell");if spellName==spell and (spellRank==rank or rank==nil) then if sw==0 then return GetSpellCooldown(ix,"spell");end;if sw==1 then if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,'spell');return ix;end;end;if sw==nil then return ix;end;end;ix=ix-1;end;end;end;ix,SE=192,0;while(ix>0) do spellName,spellRank=GetSpellName(ix,"spell");if buffed("Sun's Embrace")=="buff" then if spellName=="Sun's Embrace" and spellRank=="Rank 4" then SE=150;break;end;if spellName=="Sun's Embrace" and spellRank=="Rank 3" then SE=100;break;end;if spellName=="Sun's Embrace" and spellRank=="Rank 2" then SE=50;break;end;if spellName=="Sun's Embrace" and spellRank=="Rank 1" then SE=25;break;end;ix=ix-1;else if spellName=="Sun's Embrace" then if GetSpellCooldown(ix,"spell")~=0 then SE=-1;end;break;end;end;ix=ix-1;end;Rank=9;if Rank==9 and UnitLevel("player")>=60 then if ActSpell("Smite","Rank 9") then if UnitMana("player")>=300-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 9)");else if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;end;end;else Rank=Rank-1;end;if Rank==8 and UnitLevel("player")>=54 then if ActSpell("Smite","Rank 8") then if UnitMana("player")>=280-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 8)");else if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;end;end;else Rank=Rank-1;end;if Rank==7 and UnitLevel("player")>=46 then if ActSpell("Smite","Rank 7") then if UnitMana("player")>=230-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 7)");else if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;end;end;else Rank=Rank-1;end;if Rank==6 and UnitLevel("player")>=38 then if ActSpell("Smite","Rank 6") then if UnitMana("player")>=185-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 6)");else if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;end;end;else Rank=Rank-1;end;if Rank==5 and UnitLevel("player")>=30 then if ActSpell("Smite","Rank 5") then if UnitMana("player")>=140-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 5)");else if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;end;end;else Rank=Rank-1;end;if Rank==4 and UnitLevel("player")>=22 then if ActSpell("Smite","Rank 4") then if UnitMana("player")>=95-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 4)");else if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;end;end;else Rank=Rank-1;end;if Rank==3 and UnitLevel("player")>=14 then if ActSpell("Smite","Rank 3") then if UnitMana("player")>=60-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 3)");else if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;end;end;else Rank=Rank-1;end;if Rank==2 and UnitLevel("player")>=6 then if ActSpell("Smite","Rank 2") then if UnitMana("player")>=30-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 2)");else if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;end;end;else Rank=Rank-1;end;if Rank==1 and UnitLevel("player")>=1 then if ActSpell("Smite","Rank 1") then if UnitMana("player")>=20-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 1)");else if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;end;end;else Rank=Rank-1;end;
+-=Smite+Sun's Embrace=- 2
+/script local SE,Rank,spellName,spellRank=0;if nil then CastSpellByName("Smite");end;local function ActSpell(spell,rank,sw) local ix=192;while(ix>0) do if ix==0 then return nil;else spellName,spellRank=GetSpellName(ix,"spell");if spellName==spell and (spellRank==rank or rank==nil) then if sw==true then return ix;end;if sw==0 then return GetSpellCooldown(ix,"spell");end;if sw==nil then if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,'spell');return ix;end;end;end;ix=ix-1;end;end;if sw==0 then return 9;end;end;local function GotBuff(name,target) local tex,cnt,ix;for ix = 1,32 do tex,cnt = UnitBuff("player",ix);if not tex then return end;if strfind(tex,name) then return cnt end;end;end;if GotBuff("Spell_Holy_HolyProtection") then local ix,SE=64,0;while(ix>0) do spellName,spellRank=GetSpellName(ix,"spell");if spellName=="Sun's Embrace" and spellRank=="Rank 4" then SE=150;break;end;if spellName=="Sun's Embrace" and spellRank=="Rank 3" then SE=100;break;end;if spellName=="Sun's Embrace" and spellRank=="Rank 2" then SE=50;break;end;if spellName=="Sun's Embrace" and spellRank=="Rank 1" then SE=25;break;end;ix=ix-1;end;end;function castSmite()if ActSpell("Smite","Rank 9",true) then if UnitMana("player")>=300-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 9");return;else if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;end;end; if ActSpell("Smite","Rank 8",true) then if UnitMana("player")>=280-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 8");return;else if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;end;end; if ActSpell("Smite","Rank 7",true) then if UnitMana("player")>=230-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 7");return;else if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;end;end; if ActSpell("Smite","Rank 6",true) then if UnitMana("player")>=185-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 6");return;else if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;end;end; if ActSpell("Smite","Rank 5",true) then if UnitMana("player")>=140-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 5");return;else if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;end;end; if ActSpell("Smite","Rank 4",true) then if UnitMana("player")>=95-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 4");return;else if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;end;end; if ActSpell("Smite","Rank 3",true) then if UnitMana("player")>=60-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 3");return;else if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;end;end; if ActSpell("Smite","Rank 2",true) then if UnitMana("player")>=30-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 2");return;else if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;end;end; if ActSpell("Smite","Rank 1",true) then if UnitMana("player")>=20-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 1");return;else if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;end;end;end;castSmite();
 
-/script local ix,SE,Rank,spellName,spellRank;
+/script local SE,Rank,spellName,spellRank=0;if nil then CastSpellByName("Smite");end;
 local function ActSpell(spell,rank,sw) 
-	ix=192;while(ix>0) do 
+	local ix=192;
+	while(ix>0) do 
 		if ix==0 then return nil;
 		else 
 			spellName,spellRank=GetSpellName(ix,"spell");
 			if spellName==spell and (spellRank==rank or rank==nil) then 
+				if sw==true then return ix;end;
 				if sw==0 then return GetSpellCooldown(ix,"spell");end;
-				if sw==1 then 
+				if sw==nil then 
 					if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,'spell');return ix;end;
 				end;
-				if sw==nil then return ix;end;
 			end;
 			ix=ix-1;
 		end;
 	end;
+	if sw==0 then return 9;end;
 end;
-ix,SE=192,0;
-while(ix>0) do 
-	spellName,spellRank=GetSpellName(ix,"spell");
-	if buffed("Sun's Embrace")=="buff" then 
+local function GotBuff(name,target) 
+	local tex,cnt,ix;
+	for ix = 1,32 do 
+		tex,cnt = UnitBuff("player",ix);
+		if not tex then return end;
+		if strfind(tex,name) then return cnt end;
+	end;
+end;
+if GotBuff("Spell_Holy_HolyProtection") then 
+	local ix,SE=64,0;
+	while(ix>0) do 
+		spellName,spellRank=GetSpellName(ix,"spell");
 		if spellName=="Sun's Embrace" and spellRank=="Rank 4" then SE=150;break;end;
 		if spellName=="Sun's Embrace" and spellRank=="Rank 3" then SE=100;break;end;
 		if spellName=="Sun's Embrace" and spellRank=="Rank 2" then SE=50;break;end;
 		if spellName=="Sun's Embrace" and spellRank=="Rank 1" then SE=25;break;end;
 		ix=ix-1;
-	else 
-		if spellName=="Sun's Embrace" then 
-			if GetSpellCooldown(ix,"spell")~=0 then SE=-1;end;
-			break;
-		end;
 	end;
-	ix=ix-1;
 end;
-Rank=9;
-if Rank==9 and UnitLevel("player")>=60 then 
-	if ActSpell("Smite","Rank 9") then 
-		if UnitMana("player")>=300-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 9)");
+function castSmite()
+	if ActSpell("Smite","Rank 9",true) then 
+		if UnitMana("player")>=300-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 9");return;
 		else 
-			if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;
+			if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;
+		end;
+	end; 
+	if ActSpell("Smite","Rank 8",true) then 
+		if UnitMana("player")>=280-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 8");return;
+		else 
+			if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;
+		end;
+	end; 
+	if ActSpell("Smite","Rank 7",true) then 
+		if UnitMana("player")>=230-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 7");return;
+		else 
+			if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;
+		end;
+	end; 
+	if ActSpell("Smite","Rank 6",true) then 
+		if UnitMana("player")>=185-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 6");return;
+		else 
+			if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;
+		end;
+	end; 
+	if ActSpell("Smite","Rank 5",true) then 
+		if UnitMana("player")>=140-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 5");return;
+		else 
+			if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;
+		end;
+	end; 
+	if ActSpell("Smite","Rank 4",true) then 
+		if UnitMana("player")>=95-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 4");return;
+		else 
+			if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;
+		end;
+	end; 
+	if ActSpell("Smite","Rank 3",true) then 
+		if UnitMana("player")>=60-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 3");return;
+		else 
+			if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;
+		end;
+	end; 
+	if ActSpell("Smite","Rank 2",true) then 
+		if UnitMana("player")>=30-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 2");return;
+		else 
+			if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;
+		end;
+	end; 
+	if ActSpell("Smite","Rank 1",true) then 
+		if UnitMana("player")>=20-SE or GotBuff("Spell_Frost_WindWalkOn") then ActSpell("Smite","Rank 1");return;
+		else 
+			if ActSpell("Sun's Embrace",nil,0)==0 then ActSpell("Sun's Embrace");return;end;
 		end;
 	end;
-else Rank=Rank-1;end;
-if Rank==8 and UnitLevel("player")>=54 then 
-	if ActSpell("Smite","Rank 8") then 
-		if UnitMana("player")>=280-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 8)");
-		else 
-			if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;
-		end;
-	end;
-else Rank=Rank-1;end;
-if Rank==7 and UnitLevel("player")>=46 then 
-	if ActSpell("Smite","Rank 7") then 
-		if UnitMana("player")>=230-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 7)");
-		else 
-			if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;
-		end;
-	end;
-else Rank=Rank-1;end;
-if Rank==6 and UnitLevel("player")>=38 then 
-	if ActSpell("Smite","Rank 6") then 
-		if UnitMana("player")>=185-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 6)");
-		else 
-			if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;
-		end;
-	end;
-else Rank=Rank-1;end;
-if Rank==5 and UnitLevel("player")>=30 then 
-	if ActSpell("Smite","Rank 5") then 
-		if UnitMana("player")>=140-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 5)");
-		else 
-			if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;
-		end;
-	end;
-else Rank=Rank-1;end;
-if Rank==4 and UnitLevel("player")>=22 then 
-	if ActSpell("Smite","Rank 4") then 
-		if UnitMana("player")>=95-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 4)");
-		else 
-			if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;
-		end;
-	end;
-else Rank=Rank-1;end;
-if Rank==3 and UnitLevel("player")>=14 then 
-	if ActSpell("Smite","Rank 3") then 
-		if UnitMana("player")>=60-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 3)");
-		else 
-			if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;
-		end;
-	end;
-else Rank=Rank-1;end;
-if Rank==2 and UnitLevel("player")>=6 then 
-	if ActSpell("Smite","Rank 2") then 
-		if UnitMana("player")>=30-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 2)");
-		else 
-			if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;
-		end;
-	end;
-else Rank=Rank-1;end;
-if Rank==1 and UnitLevel("player")>=1 then 
-	if ActSpell("Smite","Rank 1") then 
-		if UnitMana("player")>=20-SE or buffed("Inner Focus")=="buff" then CastSpellByName("Smite(Rank 1)");
-		else 
-			if SE==0 then ActSpell("Sun's Embrace",nil,1);else Rank=Rank-1;end;
-		end;
-	end;
-else Rank=Rank-1;end;
+end;
+castSmite();
