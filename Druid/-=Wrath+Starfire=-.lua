ac4f83165d8fa52886fbf1d7abea97c47314ba43
Ulukay <95168076+UlakayOutcast@users.noreply.github.com>
2025-07-09 20:02:56 +0700

    Add files via upload

--- Druid/-=Wrath+Starfire=-.lua
+++ Druid/-=Wrath+Starfire=-.lua
@@ -1,32 +1,32 @@
 -=Wrath+Starfire=-
 
-/script local SE,Rank,spellName,spellRank=0;if nil then CastSpellByName("Wrath");end;local function GotBuff(name,target) local tex,cnt,ix;for ix = 1,32 do tex,cnt = UnitBuff("player",ix);if not tex then return end;if strfind(tex,name) then return cnt end;end;end;local function ActSpell(spell,rank,sw) local ix=192;while(ix>0) do if ix==0 then return nil;else spellName,spellRank=GetSpellName(ix,"spell");if spellName==spell and (spellRank==rank or rank==nil) then if sw==true then return ix;end;if sw==0 then return GetSpellCooldown(ix,"spell");end;if sw==nil then if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,'spell');return ix;end;end;end;ix=ix-1;end;end;end;function CastSpels()if not UnitCanAttack("player","target") or UnitHealth("target")==0 then TargetNearestEnemy();end;if UnitCanAttack("player","target") and UnitHealth("target")>0 then local GB=(GotBuff("Spell_Arcane_StarFire") or GotBuff("Spell_Nature_NaturesBlessing"));local ASF=1;if GotBuff("Spell_Nature_AbolishMagic") then ASF=0.5;end;local PM=UnitMana("player");local TH=UnitHealth("target");if GB and (PM>=340 or OoC) and TH>=496 and ActSpell("Starfire","Rank 7") then return;end;if GB and (PM>=315 or OoC) and TH>=445 and ActSpell("Starfire","Rank 6") then return;end;if GB and (PM>=275 or OoC) and TH>=362 and ActSpell("Starfire","Rank 5") then return;end;if (PM>=210*ASF or OoC) and TH>=292 and ActSpell("Wrath","Rank 9") then return;end;if GB and (PM>=230 or OoC) and TH>=280 and ActSpell("Starfire","Rank 4") then return;end;if (PM>=180*ASF or OoC) and TH>=248 and ActSpell("Wrath","Rank 8") then return;end;if GB and (PM>=180 or OoC) and TH>=201 and ActSpell("Starfire","Rank 3") then return;end;if (PM>=155*ASF or OoC) and TH>=192 and ActSpell("Wrath","Rank 7") then return;end;if (PM>=125*ASF or OoC) and TH>=146 and ActSpell("Wrath","Rank 6") then return;end;if GB and (PM>=135 or OoC) and TH>=137 and ActSpell("Starfire","Rank 2") then return;end;if (PM>=100*ASF or OoC) and TH>=106 and ActSpell("Wrath","Rank 5") then return;end;if GB and (PM>=95 or OoC) and TH>=89 and ActSpell("Starfire","Rank 1") then return;end;if (PM>=70*ASF or OoC) and TH>=66 and ActSpell("Wrath","Rank 4") then return;end;if (PM>=55*ASF or OoC) and TH>=46 and ActSpell("Wrath","Rank 3") then return;end;if (PM>=35*ASF or OoC) and TH>=26 and ActSpell("Wrath","Rank 2") then return;end;if (PM>=20*ASF or OoC) and TH>=13 and ActSpell("Wrath","Rank 1") then return;end;end;end;CastSpels();
+/script local function GotBuff(name,target) if not target then target="player";end;local tex,cnt,ix;for ix = 1,32 do tex,cnt = UnitBuff(target,ix);if not tex then return; end;if strfind(tex,name) then return cnt; end;end;end;local function ActSpell(spell,rank,sw) local ix,spellName,spellRank=192;while(ix>0) do spellName,spellRank=GetSpellName(ix,"spell");if spellName==spell and (spellRank==rank or rank==nil) then if sw==true then return ix;end;if sw==0 then return GetSpellCooldown(ix,"spell");end;if sw=="self" then if rank then CastSpellByName(spell.."("..rank..")",1) else CastSpellByName(spell,1);end;return ix;end;if sw==nil then if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,"spell");return ix;end;end;end;ix=ix-1;end;end;function CastSpels()if not UnitCanAttack("player","target") or UnitHealth("target")==0 then TargetNearestEnemy();end;if UnitCanAttack("player","target") and UnitHealth("target")>0 then local GB=(GotBuff("Spell_Arcane_StarFire") or GotBuff("Spell_Nature_NaturesBlessing"));local ASF=1;if GotBuff("Spell_Nature_AbolishMagic") then ASF=0.5;end;local PM=UnitMana("player");local TH=UnitHealth("target");local OoC=GotBuff("Spell_Shadow_ManaBurn")if GB and (PM>=340 or OoC) and TH>=496 and ActSpell("Starfire","Rank 7") then return;end;if GB and (PM>=315 or OoC) and TH>=445 and ActSpell("Starfire","Rank 6") then return;end;if GB and (PM>=275 or OoC) and TH>=362 and ActSpell("Starfire","Rank 5") then return;end;if (PM>=210*ASF or OoC) and TH>=292 and ActSpell("Wrath","Rank 9") then return;end;if GB and (PM>=230 or OoC) and TH>=280 and ActSpell("Starfire","Rank 4") then return;end;if (PM>=180*ASF or OoC) and TH>=248 and ActSpell("Wrath","Rank 8") then return;end;if GB and (PM>=180 or OoC) and TH>=201 and ActSpell("Starfire","Rank 3") then return;end;if (PM>=155*ASF or OoC) and TH>=192 and ActSpell("Wrath","Rank 7") then return;end;if (PM>=125*ASF or OoC) and TH>=146 and ActSpell("Wrath","Rank 6") then return;end;if GB and (PM>=135 or OoC) and TH>=137 and ActSpell("Starfire","Rank 2") then return;end;if (PM>=100*ASF or OoC) and TH>=106 and ActSpell("Wrath","Rank 5") then return;end;if GB and (PM>=95 or OoC) and TH>=89 and ActSpell("Starfire","Rank 1") then return;end;if (PM>=70*ASF or OoC) and TH>=66 and ActSpell("Wrath","Rank 4") then return;end;if (PM>=55*ASF or OoC) and TH>=46 and ActSpell("Wrath","Rank 3") then return;end;if (PM>=35*ASF or OoC) and TH>=26 and ActSpell("Wrath","Rank 2") then return;end;if (PM>=20*ASF or OoC) and TH>=13 and ActSpell("Wrath","Rank 1") then return;end;CastSpellByName("Wrath");end;end;CastSpels();
 
 /script 
-local SE,Rank,spellName,spellRank=0;if nil then CastSpellByName("Wrath");end;
 local function GotBuff(name,target) 
+	if not target then target="player";end;
 	local tex,cnt,ix;
 	for ix = 1,32 do 
-		tex,cnt = UnitBuff("player",ix);
-		if not tex then return end;
-		if strfind(tex,name) then return cnt end;
+		tex,cnt = UnitBuff(target,ix);
+		if not tex then return; end;
+		if strfind(tex,name) then return cnt; end;
 	end;
 end;
 local function ActSpell(spell,rank,sw) 
-	local ix=192;
+	local ix,spellName,spellRank=192;
 	while(ix>0) do 
-		if ix==0 then return nil;
-		else 
-			spellName,spellRank=GetSpellName(ix,"spell");
-			if spellName==spell and (spellRank==rank or rank==nil) then 
-				if sw==true then return ix;end;
-				if sw==0 then return GetSpellCooldown(ix,"spell");end;
-				if sw==nil then 
-					if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,'spell');return ix;end;
-				end;
+		spellName,spellRank=GetSpellName(ix,"spell");
+		if spellName==spell and (spellRank==rank or rank==nil) then 
+			if sw==true then return ix;end;
+			if sw==0 then return GetSpellCooldown(ix,"spell");end;
+			if sw=="self" then 
+				if rank then CastSpellByName(spell.."("..rank..")",1) else CastSpellByName(spell,1);end;return ix;
 			end;
-			ix=ix-1;
+			if sw==nil then 
+				if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,"spell");return ix;end;
+			end;
 		end;
+		ix=ix-1;
 	end;
 end;
 function CastSpels()
@@ -52,6 +52,7 @@ function CastSpels()
 		if (PM>=55*ASF or OoC) and TH>=46 and ActSpell("Wrath","Rank 3") then return;end;
 		if (PM>=35*ASF or OoC) and TH>=26 and ActSpell("Wrath","Rank 2") then return;end;
 		if (PM>=20*ASF or OoC) and TH>=13 and ActSpell("Wrath","Rank 1") then return;end;
+		CastSpellByName("Wrath");
 	end;
 end;
 CastSpels();
--- Druid/Druid-=Regrowth+sw=- v2.lua
+++ Druid/Druid-=Regrowth+sw=- v2.lua
@@ -1,31 +1,31 @@
 -=Regrowth=- +Swiftmend Rank в зависимости от уровня играка, его маны, уровня цели и отсутствующего здоровья цели. С зажатым Alt используется "Nature's Swiftness"
-/script local function GotBuff(name,target) if not target then target="target";end;local tex,cnt,ix;for ix = 1,32 do tex,cnt = UnitBuff(target,ix) if not tex then return end;if strfind(tex,name) then return cnt end;end;end;local function ActSpell(spell,rank,sw) local ix=192;while(ix>0) do if ix==0 then return nil;else spellName,spellRank=GetSpellName(ix,"spell");if spellName==spell and (spellRank==rank or rank==nil) then if sw==true then return ix;end;if sw==0 then return GetSpellCooldown(ix,"spell");end;if sw==nil then if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,"spell");return ix;end;end;end;ix=ix-1;end;end;end;local function CastSpels()if UnitLevel("target")==0 or UnitCanAttack("player","target") then UT="player";else UT="target";end;local PL=UnitLevel("player");local TL=UnitLevel(UT);local PM=UnitMana("player");local AH=UnitHealthMax(UT)-UnitHealth(UT);local SMB=GotBuff("Spell_Shadow_ManaBurn","player");if IsAltKeyDown( and ActSpell("Nature's Swiftness") then return;end;if GotBuff("ResistNature",UT and ActSpell("Swiftmend") then return;end;if PL>59 and TL>53 and (PM>879 or SMB) and AH>2067 and ActSpell("Regrowth","Rank 9") then return; end;if PL>53 and TL>47 and (PM>739 or SMB) and AH>1670 and ActSpell("Regrowth","Rank 8") then return; end;if PL>47 and TL>41 and (PM>614 or SMB) and AH>1332 and ActSpell("Regrowth","Rank 7") then return; end;if PL>41 and TL>35 and (PM>509 or SMB) and AH>1057 and ActSpell("Regrowth","Rank 6") then return; end;if PL>35 and TL>29 and (PM>419 or SMB) and AH>832 and ActSpell("Regrowth","Rank 5") then return; end;if PL>29 and TL>23 and (PM>349 or SMB) and AH>661 and ActSpell("Regrowth","Rank 4") then return; end;if PL>23 and TL>17 and (PM>279 or SMB) and AH>449 and ActSpell("Regrowth","Rank 3") then return; end;if PL>17 and TL>11 and (PM>204 or SMB) and AH>339 and ActSpell("Regrowth","Rank 2") then return; end;if PL>11 and TL>0 and (PM>119 or SMB) and AH>182 and ActSpell("Regrowth","Rank 1") then return; end;if UnitCanAttack("player","target") then CastSpellByName("Regrowth");end;end;CastSpels();
+/script local function GotBuff(name,target) if not target then target="target";end;local tex,cnt,ix;for ix = 1,32 do tex,cnt = UnitBuff(target,ix);if not tex then return; end;if strfind(tex,name) then return cnt; end;end;end;local function ActSpell(spell,rank,sw) local ix,spellName,spellRank=192;while(ix>0) do spellName,spellRank=GetSpellName(ix,"spell");if spellName==spell and (spellRank==rank or rank==nil) then if sw==true then return ix;end;if sw==0 then return GetSpellCooldown(ix,"spell");end;if sw=="self" then if rank then CastSpellByName(spell.."("..rank..")",1) else CastSpellByName(spell,1);end;return ix;end;if sw==nil then if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,"spell");return ix;end;end;end;ix=ix-1;end;end;local function CastSpels()if UnitLevel("target")==0 or UnitCanAttack("player","target") then UT="player";else UT="target";end;local PL=UnitLevel("player");local TL=UnitLevel(UT);local PM=UnitMana("player");local AH=UnitHealthMax(UT)-UnitHealth(UT);local SMB=GotBuff("Spell_Shadow_ManaBurn","player");if IsAltKeyDown( and ActSpell("Nature's Swiftness") then return;end;if GotBuff("ResistNature",UT and ActSpell("Swiftmend") then return;end;if PL>59 and TL>53 and (PM>879 or SMB) and AH>2067 and ActSpell("Regrowth","Rank 9") then return; end;if PL>53 and TL>47 and (PM>739 or SMB) and AH>1670 and ActSpell("Regrowth","Rank 8") then return; end;if PL>47 and TL>41 and (PM>614 or SMB) and AH>1332 and ActSpell("Regrowth","Rank 7") then return; end;if PL>41 and TL>35 and (PM>509 or SMB) and AH>1057 and ActSpell("Regrowth","Rank 6") then return; end;if PL>35 and TL>29 and (PM>419 or SMB) and AH>832 and ActSpell("Regrowth","Rank 5") then return; end;if PL>29 and TL>23 and (PM>349 or SMB) and AH>661 and ActSpell("Regrowth","Rank 4") then return; end;if PL>23 and TL>17 and (PM>279 or SMB) and AH>449 and ActSpell("Regrowth","Rank 3") then return; end;if PL>17 and TL>11 and (PM>204 or SMB) and AH>339 and ActSpell("Regrowth","Rank 2") then return; end;if PL>11 and TL>0 and (PM>119 or SMB) and AH>182 and ActSpell("Regrowth","Rank 1") then return; end;if UnitCanAttack("player","target") then CastSpellByName("Regrowth");end;end;CastSpels();
 -=-
 /script 
 local function GotBuff(name,target) 
 	if not target then target="target";end;
 	local tex,cnt,ix;
 	for ix = 1,32 do 
-		tex,cnt = UnitBuff(target,ix) 
-		if not tex then return end;
-		if strfind(tex,name) then return cnt end;
+		tex,cnt = UnitBuff(target,ix);
+		if not tex then return; end;
+		if strfind(tex,name) then return cnt; end;
 	end;
 end;
 local function ActSpell(spell,rank,sw) 
-	local ix=192;
+	local ix,spellName,spellRank=192;
 	while(ix>0) do 
-		if ix==0 then return nil;
-		else 
-			spellName,spellRank=GetSpellName(ix,"spell");
-			if spellName==spell and (spellRank==rank or rank==nil) then 
-				if sw==true then return ix;end;
-				if sw==0 then return GetSpellCooldown(ix,"spell");end;
-				if sw==nil then 
-					if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,"spell");return ix;end;
-				end;
+		spellName,spellRank=GetSpellName(ix,"spell");
+		if spellName==spell and (spellRank==rank or rank==nil) then 
+			if sw==true then return ix;end;
+			if sw==0 then return GetSpellCooldown(ix,"spell");end;
+			if sw=="self" then 
+				if rank then CastSpellByName(spell.."("..rank..")",1) else CastSpellByName(spell,1);end;return ix;
 			end;
-			ix=ix-1;
+			if sw==nil then 
+				if GetSpellCooldown(ix,"spell")==0 then CastSpell(ix,"spell");return ix;end;
+			end;
 		end;
+		ix=ix-1;
 	end;
 end;
 local function CastSpels()
