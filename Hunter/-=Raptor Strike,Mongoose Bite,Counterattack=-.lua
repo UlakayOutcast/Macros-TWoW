fbd16136b0ae0d21c9d6f29abeb5ea5264ed40d6
Ulukay <95168076+UlakayOutcast@users.noreply.github.com>
2025-08-12 16:28:51 +0700

    Add files via upload

--- Hunter/-=Raptor Strike,Mongoose Bite,Counterattack=-.lua
+++ Hunter/-=Raptor Strike,Mongoose Bite,Counterattack=-.lua
@@ -26,7 +26,7 @@ end;
 
 
 -=ARMC=- С проверкой отката. SuperMacro
-/script local A,R,M,C,X;for X=1,192 do if IsAttackAction(X)then A=X;end;if GetSpellName(X,"spell")=="Raptor Strike"then R=X;end;if GetSpellName(X,"spell")=="Mongoose Bite"then M=X;end;if GetSpellName(X,"spell")=="Counterattack"then C=X;end;end;if not IsCurrentAction(A) then UseAction(A);end;if R and GetSpellCooldown(R,"spell")==0 then CastSpellByName("Raptor Strike","spell")end;if M and GetSpellCooldown(M,"spell")==0 then CastSpellByName("Mongoose Bite","spell")end;if C and GetSpellCooldown(C,"spell")==0 then CastSpellByName("Counterattack","spell")end;
+/script local A,R,M,C,X;for X=1,192 do if IsAttackAction(X)then A=X;end;if GetSpellName(X,"spell")=="Raptor Strike"then R=X;end;if GetSpellName(X,"spell")=="Mongoose Bite"then M=X;end;if GetSpellName(X,"spell")=="Counterattack"then C=X;end;end;if not IsCurrentAction(A) then UseAction(A);end;if R and GetSpellCooldown(R,"spell")==0 then CastSpellByName("Raptor Strike")end;if M and GetSpellCooldown(M,"spell")==0 then CastSpellByName("Mongoose Bite")end;if C and GetSpellCooldown(C,"spell")==0 then CastSpellByName("Counterattack")end;
 
 /script local A,R,M,C,X;
 for X=1,192 do 
@@ -36,14 +36,14 @@ for X=1,192 do
 	if GetSpellName(X,"spell")=="Counterattack"then C=X;end;
 end;
 if not IsCurrentAction(A) then UseAction(A);end;
-if R and GetSpellCooldown(R,"spell")==0 then CastSpellByName("Raptor Strike","spell")end;
-if M and GetSpellCooldown(M,"spell")==0 then CastSpellByName("Mongoose Bite","spell")end;
-if C and GetSpellCooldown(C,"spell")==0 then CastSpellByName("Counterattack","spell")end;
+if R and GetSpellCooldown(R,"spell")==0 then CastSpellByName("Raptor Strike")end;
+if M and GetSpellCooldown(M,"spell")==0 then CastSpellByName("Mongoose Bite")end;
+if C and GetSpellCooldown(C,"spell")==0 then CastSpellByName("Counterattack")end;
 
 
 
 -=ARMC=- С проверкой отката и подстановкой ранга. SuperMacro
-/script local N,R,C;local UL=UnitLevel;if UnitExists("target")==nil then TargetNearestEnemy();end;for C=1,72 do if IsAttackAction(C)then if not IsCurrentAction(C)then UseAction(C);end;C=72;end;end;if UL("player")>55 then LV="Rank 8" else if UL("player")>47 then LV="Rank 7" else if UL("player")>39 then LV="Rank 6" else if UL("player")>31 then LV="Rank 5" else if UL("player")>23 then LV="Rank 4" else if UL("player")>15 then LV="Rank 3" else if UL("player")>7 then LV="Rank 2" else if UL("player")>0 then LV="Rank 1" else LV="0" end;end;end;end;end;end;end;end; for C=1,200,1 do N,R=GetSpellName(C,"spell");if N=="Raptor Strike" and R==LV and GetSpellCooldown(C,"spell")==0 then CastSpellByName("Raptor Strike("..LV..")");C=200;end;end;if UL("player")>57 then LV="Rank 4" else if UL("player")>43 then LV="Rank 3" else if UL("player")>29 then LV="Rank 2" else if UL("player")>15 then LV="Rank 1" else LV="0" end;end;end;end;CastSpellByName("Mongoose Bite("..LV..")");if UL("player")>53 then LV="Rank 3" else if UL("player")>41 then LV="Rank 2" else if UL("player")>30 then LV="Rank 1" else LV="0" end;end;end;CastSpellByName("Counterattack("..LV..")");
+/script local N,R,C;local UL=UnitLevel;if UnitExists("target")==nil then TargetNearestEnemy();end;for C=1,72 do if IsAttackAction(C)then if not IsCurrentAction(C)then UseAction(C);end;C=72;end;end;if UL("player")>55 then LV="Rank 8" else if UL("player")>47 then LV="Rank 7" else if UL("player")>39 then LV="Rank 6" else if UL("player")>31 then LV="Rank 5" else if UL("player")>23 then LV="Rank 4" else if UL("player")>15 then LV="Rank 3" else if UL("player")>7 then LV="Rank 2" else if UL("player")>0 then LV="Rank 1" else LV="0" end;end;end;end;end;end;end;end; for C=1,200,1 do N,R=GetSpellName(C,"spell");if N=="Raptor Strike" and R==LV and GetSpellCooldown(C,"spell")==0 then CastSpellByName("Raptor Strike("..LV..")");break;end;end;if UL("player")>57 then LV="Rank 4" else if UL("player")>43 then LV="Rank 3" else if UL("player")>29 then LV="Rank 2" else if UL("player")>15 then LV="Rank 1" else LV="0" end;end;end;end;for C=1,200,1 do N,R=GetSpellName(C,"spell");if N=="Raptor Strike" and R==LV and GetSpellCooldown(C,"spell")==0 then CastSpellByName("Mongoose Bite("..LV..")");break;end;end;if UL("player")>53 then LV="Rank 3" else if UL("player")>41 then LV="Rank 2" else if UL("player")>30 then LV="Rank 1" else LV="0" end;end;end;for C=1,200,1 do N,R=GetSpellName(C,"spell");if N=="Raptor Strike" and R==LV and GetSpellCooldown(C,"spell")==0 then CastSpellByName("Counterattack("..LV..")");break;end;end;
 
 /script local N,R,C;local UL=UnitLevel;
 if UnitExists("target")==nil then TargetNearestEnemy();end;
@@ -63,7 +63,7 @@ if UL("player")>0 then LV="Rank 1" else LV="0"
 end;end;end;end;end;end;end;end; 
 for C=1,200,1 do N,R=GetSpellName(C,"spell");
 	if N=="Raptor Strike" and R==LV and GetSpellCooldown(C,"spell")==0 then 
-		CastSpellByName("Raptor Strike("..LV..")");C=200;
+		CastSpellByName("Raptor Strike("..LV..")");break;
 	end;
 end;
 if UL("player")>57 then LV="Rank 4" else 
@@ -71,9 +71,17 @@ if UL("player")>43 then LV="Rank 3" else
 if UL("player")>29 then LV="Rank 2" else 
 if UL("player")>15 then LV="Rank 1" else LV="0" 
 end;end;end;end;
-CastSpellByName("Mongoose Bite("..LV..")");
+for C=1,200,1 do N,R=GetSpellName(C,"spell");
+	if N=="Raptor Strike" and R==LV and GetSpellCooldown(C,"spell")==0 then 
+		CastSpellByName("Mongoose Bite("..LV..")");break;
+	end;
+end;
 if UL("player")>53 then LV="Rank 3" else 
 if UL("player")>41 then LV="Rank 2" else 
 if UL("player")>30 then LV="Rank 1" else LV="0" 
 end;end;end;
-CastSpellByName("Counterattack("..LV..")");
+for C=1,200,1 do N,R=GetSpellName(C,"spell");
+	if N=="Raptor Strike" and R==LV and GetSpellCooldown(C,"spell")==0 then 
+		CastSpellByName("Counterattack("..LV..")");break;
+	end;
+end;
