afd72d25fa1abef0ae3c432a173bb2748f11ec27
Ulukay <95168076+UlakayOutcast@users.noreply.github.com>
2026-02-08 02:03:54 +0700

    Add files via upload

---
+++ Mage/-=Icicles + Cold Snap=-.lua
@@ -0,0 +1,13 @@
+-=Icicles + Cold Snap=-
+
+/script local ix;for ix=1,199 do if GetSpellName(ix,"spell")=="Icicles"then if GetSpellCooldown(ix,"spell")==0 then CastSpellByName("Icicles"); else CastSpellByName("Cold Snap");end;break;end;end;
+
+/script 
+local ix;
+for ix=1,199 do 
+	if GetSpellName(ix,"spell")=="Icicles"then print("0")
+		if GetSpellCooldown(ix,"spell")==0 then CastSpellByName("Icicles"); 
+		else CastSpellByName("Cold Snap");end;
+		break;
+	end;
+end;
